3cf7425945f0fc585835accbff4b09354b67dc09
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2025-08-23 12:46:49 +0530

    Create Users Table

--- test/src/Users.js
+++ test/src/Users.js
@@ -1,4 +1,17 @@
+import { Box } from "@mui/material";
 import UserForm from "./UserForm";
+import UsersTable from "./UsersTable";
+
+const users = [
+    {
+        id: 1,
+        name: 'Kumara',
+    },
+    {
+        id: 2,
+        name: 'Kasun'
+    }
+];
 
 const Users = () => {
     return (
